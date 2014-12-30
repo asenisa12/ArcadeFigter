4a2d173d343ddf688402b22c7cf9add56c154b29
asenisa12 <asen0stoilov@gmail.com>
2014-12-30 18:42:07 +0200

    adding Grid.h

---
+++ FightingGame/SourceCode/Grid.h
@@ -0,0 +1,64 @@
+#ifndef SQUARE_GRID
+#define SQUARE_GRID
+
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+namespace grid
+{
+	static const int OCCUPATION_PRIORITY = 5;
+	static const int FREE_PRIORITY = 1;
+	static const int GRID_SIZE_X = 32;
+	static const int GRID_SIZE_Y = 5;
+
+	struct Location
+	{
+		int X;
+		int Y;
+		bool operator==(Location const &b)const
+		{
+			return b.X == X && Y == b.Y;
+		}
+		bool operator!=(Location const &b)const
+		{
+			return b.X != X || Y != b.Y;
+		}
+		
+	};
+
+	struct LocationHash
+	{
+		typedef std::hash<int> hashForInt;
+		size_t operator()(Location const& key)const;
+
+	};
+
+	struct Equal
+	{
+		bool operator()(const Location &id1, const Location &id2) const;
+	};
+
+	class SquareGrid
+	{
+
+		enum{COST_ID=1, NEIGHBORS_ID=0};
+
+		int length, hight;
+		int squareSize;
+		Location squares[GRID_SIZE_Y][GRID_SIZE_X];
+		std::unordered_map<Location, std::tuple<std::vector<Location*>,int>, LocationHash, Equal> edges;
+		void add_neighbors(int row, int col);
+		void create_grid();
+	public:
+
+		SquareGrid(int length_, int hight_);
+		int cost(Location id);
+		void change_cost(Location id, int cost);
+		const std::vector<Location*> neighbors(Location id);
+	};
+
+
+}
+
+#endif
